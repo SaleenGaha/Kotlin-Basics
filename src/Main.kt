6bfdb29b4fff81ee7cd447d95be694500eb5e141
SaleenGaha <you@example.com>
2025-06-26 15:17:30 +0545

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,36 @@
+
+fun main(){
+    println("hello world")
+    println(1+2)
+    print(false)
+}
+
+
+/*print()
+Does not add a newline at the end.
+
+Keeps the cursor on the same line after printing.
+🔹 println()
+Adds a newline after the text.
+
+Moves the cursor to the next line.
+
+Example of print
+println("Hello")
+println("World")
+
+output:
+HelloWorld
+
+
+example of println
+print("Hello")
+print("World")
+output:
+Hello
+World
+
+Use print() if you want to continue output on the same line.
+
+Use println() if you want each output on a new line.
+        */
