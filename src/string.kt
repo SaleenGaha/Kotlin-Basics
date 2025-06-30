ea921841a78c5b50c2e90ae39c18b6fff42cf588
SaleenGaha <you@example.com>
2025-06-30 18:40:46 +0545

    string literal like raw and escape string

---
+++ src/string.kt
@@ -0,0 +1,33 @@
+//fun  main(){
+//    val str="hello"
+//    print(str)
+//    print(" ")
+//    print(str[2])
+//}
+//string concanetation
+//fun main(){
+//    var str1="hello"
+//
+//    var str2=" saleen"
+//    print(str1 + str2)
+//    print(" this is " + str1 +str2)
+//    println("100"+50) // output is 10050
+//    print(50 +"100")  // error cannot conctenate interger with string
+//}
+
+
+// STRING LITERALS
+   // 1.escape string
+//fun main()
+//{
+//    println("Hello\nWorld")
+//    println("Hello\tWorld")
+//
+//}
+
+//Raw string wrtitten under """ quotation
+fun main() {
+    println("""this is the new world of kotlin 
+        |I m new to the world """
+
+}
