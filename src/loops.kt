460eb3aa5361628ebe3ab33ef3b3df4c66214f86
SaleenGaha <you@example.com>
2025-07-02 15:30:42 +0545

    loops and stringe  in kotlin

---
+++ src/loops.kt
@@ -0,0 +1,66 @@
+// fun main()
+// {
+//     var i =1
+// while(i<5){
+//     println("saleen")
+//     i++
+// }
+//
+// }
+//The while loop runs as long as the condition is true.
+//👉 It checks the condition before executing the loop body.
+//fun main()
+//{
+//    var count=1
+//    while(count<=5)
+//    {
+//        println("hello saleen")
+//        count ++   }
+//
+//
+//}
+
+
+
+//fun main(){
+//    print("enter the number")
+//    val number=readLine()!!.toInt()
+//    var index=1;
+//    while(index<=10)
+//    {
+//        println(index*number)
+//        index ++
+//    }
+//}
+
+
+// do while
+//A do-while loop executes the block of code first, and then checks the condition.
+//This means the loop will run at least once, even if the condition is false.
+//fun main()
+//{
+//    var counter=2
+//    do{
+//        print("hello world")
+//    }
+//        while(counter<2)
+//}
+//
+
+//for loop
+//fun main(){
+//
+//    for(i in 1..5 step 2)
+//    {
+//        println(i)
+//    }
+//}
+
+//??logic of multiolication of number
+fun main()
+{
+    var number=2;
+    for(i in 1..10){
+        println("$number*$i=${number*i}")
+    }
+}
